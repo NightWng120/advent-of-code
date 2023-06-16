e02969f35e76ff5321c7014a514a39cb20550fbe
NightWng120 <treverholmes9@gmail.com>
2023-06-15 22:46:59 -0400

    day6

--- day6/packet.py
+++ day6/packet.py
@@ -1,20 +1,24 @@
 with open('data.txt', 'r') as f:
     data = f.readline()
-
 buffer = []
+processed = []
 length = 0
 
 for index, char in enumerate(data):
-
     if len(buffer) == 4:
-        length = index + 1
+        length = index
+        processed.extend(buffer)
         break
     elif char not in buffer:
         buffer.append(char)
     elif char in buffer:
         # print(f'{char} is in {buffer}')
+        processed.extend(buffer)
         buffer = []
         buffer.append(char)
 
+# print(data)
+print(len(processed))
+print(processed)
 print(buffer)
 print(length)
